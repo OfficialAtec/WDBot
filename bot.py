b07fd2a4fe97942b6562e5103270e8e28bc142da
Jeroen B <jeroenb04@live.com>
2021-03-22 22:04:36 +0100

    Added bug, suggestie & reportcid command
    Alles gaat via settings.py, dus verander deze naar je eigen info voordat je de bot gebruikt.

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import time
 import login
 import settings
+import asyncio
 from discord.ext import commands
 bot = commands.Bot(command_prefix=settings.prefix)
 
@@ -12,4 +13,38 @@ async def on_ready():
 async def ping(ctx):
     await ctx.send('Pong! {0} ms'.format(round(bot.latency*1000,1)))
     
+@bot.command()
+async def test(ctx, *, arg):
+    if (ctx.channel.id == settings.suggestions):
+        await ctx.send(arg)
+
+@bot.command()
+async def bug(ctx, *, arg):
+    buglog = bot.get_channel(settings.buglog)
+    if (ctx.channel.id == settings.bugreport):
+        await  buglog.send("Bug: {0} | Door {1}".format(arg,ctx.author))   
+        await ctx.message.delete()
+    else:
+        await ctx.message.delete()        
+        msg = await ctx.send("Wilt u dit even in {0} doen, danku!".format(settings.bugkanaal))
+        await asyncio.sleep(1)
+        await msg.delete()
+
+@bot.command()
+async def suggestie(ctx, *, arg):
+    if (ctx.channel.id == settings.suggesties):
+        await  bot.get_channel(settings.suggesties).send("Suggestie: {0} | Door {1}".format(arg,ctx.author))   
+        await ctx.message.delete()
+        await message.add_reaction('\U0001F44D')
+        await message.add_reaction('\U0001F44E')
+    else:
+        await ctx.message.delete()        
+        msg = await ctx.send("Wilt u dit even in {0} doen, danku!".format(settings.suggesties))
+        await asyncio.sleep(1)
+        await msg.delete()
+
+@bot.command()
+async def reportcid(ctx):
+    await ctx.send(ctx.channel.id)
+
 bot.run(login.token)
--- settings.py
+++ settings.py
@@ -1,1 +1,6 @@
 prefix = ">"
+suggesties = 822798181523783690
+buglog = 823289500040953876
+bugreport = 822810328432771153
+bugkanaal = "#bugs"
+suggestiekanaal = "#suggesties"
